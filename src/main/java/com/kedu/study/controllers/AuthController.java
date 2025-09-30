b4a9bdc15c632773b3d47e5288022d2191efeb8c
Carrythefuture <146246027+Carrythefuture@users.noreply.github.com>
2025-09-30 10:31:35 +0900

    add CROSConfig Setting, AuthController fixed

--- src/main/java/com/kedu/study/controllers/AuthController.java
+++ src/main/java/com/kedu/study/controllers/AuthController.java
@@ -1,7 +1,11 @@
 package com.kedu.study.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.kedu.study.DTO.AuthDTO;
 import com.kedu.study.services.AuthService;
+
+import jakarta.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/auth")
@@ -18,19 +24,28 @@ public class AuthController {
 	private AuthService authService;
 	
 	@PostMapping("/login")
-	public ResponseEntity<String> login(@RequestBody AuthDTO dto){
+	public ResponseEntity<Map<String,String>> login(@RequestBody AuthDTO dto, HttpSession session){
 		System.out.println(dto.getId() + ":" + dto.getPw());
+		
 		AuthDTO result = authService.login(dto);
 		
 		String text = "";
+		
+		//우리는 dto로 받아서 null이 아니면 성공임.
+		Map<String,String> map = new HashMap<>();
 		if(result!=null) {
 			text = "success";
+			session.setAttribute("loginId", dto.getId());
+			
+			
+			map.put("loginId", dto.getId());
 		}
 		else
 		{
 			text = "fail";
 		}
-		return ResponseEntity.ok(text);
+		
+		return ResponseEntity.ok(map);
 	}
 	
 	@PostMapping("/insert")
@@ -40,4 +55,10 @@ public class AuthController {
 		return ResponseEntity.ok().build();
 	}
 	
+	@GetMapping
+	public ResponseEntity<Void> test (HttpSession session){
+		System.out.println("세션" + (String)session.getAttribute("loginId"));
+		
+		return ResponseEntity.ok().build();
+	}
 }
